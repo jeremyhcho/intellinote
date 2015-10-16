e91bf0f8947299fc9e0c449259a5842a18be080f
Hyung C Cho <hyungchulcho93@gmail.com>
2015-10-16 15:48:04 -0700

    test

--- app/assets/javascripts/intellinote.js.jsx
+++ app/assets/javascripts/intellinote.js.jsx
@@ -1,3 +1,52 @@
+var Route = ReactRouter.Route;
+var Router = ReactRouter.Router;
+var IndexRoute = ReactRouter.IndexRoute;
+var Link = ReactRouter.Link;
+var ButtonToolbar = ReactBootstrap.ButtonToolbar;
+
 $(function () {
-  $('#main').append("hello");
+  var App = React.createClass({
+    getInitialState: function() {
+      return {
+        user: LoginStore.returnUser()
+      };
+    },
+
+    componentDidMount: function() {
+      console.log('mount');
+      LoginStore.addChangeHandler(this._onChange);
+      ApiUtil.fetchCurrentUser();
+    },
+
+    _onChange: function () {
+      this.setState({user: LoginStore.returnUser()});
+    },
+
+    render: function () {
+      console.log('render');
+      var component;
+
+      if (typeof this.state.user !== "undefined" && typeof this.state.user.errors === "undefined") {
+        component = <Main user={this.state.user} />;
+      } else {
+        component = <Home />;
+      }
+
+      return (
+        <div>
+          { component }
+          { this.props.children }
+        </div>
+      );
+    }
+  });
+
+  var routes = (
+    <Route component={App}>
+      <Route path="/" components={{main: Main, home: Home}}>
+      </Route>
+    </Route>
+  )
+
+  React.render(<Router>{routes}</Router>, document.getElementById("main"));
 });
---
+++ app/assets/javascripts/test.js
@@ -0,0 +1,1 @@
+document.getElementById("main").appendChild(document.createElement("li"));
